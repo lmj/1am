a5b9be194333e24378f71e6a91a3e6d06cf9fd8b
James M. Lawrence <llmjjmll@gmail.com>
2014-11-02 18:03:04 -0500

    initial add

---
+++ 1am.lisp
@@ -0,0 +1,105 @@
+;;; Copyright (c) 2014 James M. Lawrence
+;;; 
+;;; Permission is hereby granted, free of charge, to any person
+;;; obtaining a copy of this software and associated documentation
+;;; files (the "Software"), to deal in the Software without
+;;; restriction, including without limitation the rights to use, copy,
+;;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;;; of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+;;; 
+;;; The above copyright notice and this permission notice shall be
+;;; included in all copies or substantial portions of the Software.
+;;; 
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+;;; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+;;; WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;;; DEALINGS IN THE SOFTWARE.
+
+(defpackage #:1am
+  (:use #:cl)
+  (:export #:test #:is #:signals #:run #:*tests*))
+
+(in-package #:1am)
+
+(defvar *tests* nil "A list of tests; the default argument to `run'.")
+(defvar *pass-count* nil)
+(defvar *running* nil)
+(defvar *failed-random-state* nil)
+
+(defun %shuffle (vector)
+  (loop for i downfrom (- (length vector) 1) to 1
+        do (rotatef (aref vector i) (aref vector (random (1+ i)))))
+  vector)
+
+(defun shuffle (sequence)
+  (%shuffle (map 'vector #'identity sequence)))
+
+(defun call-with-random-state (fn)
+  (let ((*random-state* (or *failed-random-state*
+                            (load-time-value (make-random-state t)))))
+    (setf *failed-random-state* (make-random-state nil))
+    (multiple-value-prog1 (funcall fn)
+      (setf *failed-random-state* nil))))
+
+(defun report (test-count pass-count)
+  (format t "~&Success: ~s test~:p, ~s check~:p.~%" test-count pass-count))
+
+(defun %run (fn test-count)
+  (let ((*pass-count* 0))
+    (multiple-value-prog1 (call-with-random-state fn)
+      (report test-count *pass-count*))))
+
+(defun run (&optional (tests *tests*))
+  "Run each test in the sequence `tests'. Default is `*tests*'."
+  (let ((*running* t))
+    (%run (lambda () (map nil #'funcall (shuffle tests)))
+          (length tests)))
+  (values))
+
+(defun call-test (name fn)
+  (format t "~&~s" name)
+  (finish-output)
+  (if *running*
+      (funcall fn)
+      (%run fn 1)))
+
+(defmacro test (name &body body)
+  "Define a test function and add it to `*tests*'."
+  `(progn
+     (defun ,name ()
+       (call-test ',name (lambda () ,@body)))
+     (pushnew ',name *tests*)
+     ',name))
+
+(defun passed ()
+  (write-char #\.)
+  ;; Checks done outside a test run are not tallied.
+  (when *pass-count*
+    (incf *pass-count*))
+  (values))
+
+(defmacro is (form)
+  "Assert that `form' evaluates to non-nil."
+  `(progn
+     (assert ,form)
+     (passed)))
+
+(defun %signals (expected fn)
+  (flet ((handler (condition)
+           (cond ((typep condition expected)
+                  (passed)
+                  (return-from %signals (values)))
+                 (t (error "Expected to signal ~s, but got ~s:~%~a"
+                           expected (type-of condition) condition)))))
+    (handler-bind ((condition #'handler))
+      (funcall fn)))
+  (error "Expected to signal ~s, but got nothing." expected))
+
+(defmacro signals (condition &body body)
+  "Assert that `body' signals a condition of type `condition'."
+  `(%signals ',condition (lambda () ,@body)))
---
+++ README.md
@@ -0,0 +1,114 @@
+# 1am
+
+A minimal testing framework for Common Lisp.
+
+## Synopsis
+
+The entire API consists of: `test`, `is`, `signals`, `run`, and
+`*tests*`.
+
+    (defpackage :example (:use :cl :1am))
+    (in-package :example)
+    
+    (test foo-test
+      (is (= 1 1))
+      (is (zerop 0)))
+    
+    (test bar-test
+      (signals simple-error
+        (error "bar!")))
+
+Running:
+
+    EXAMPLE> (in-package :example)
+    #<PACKAGE "EXAMPLE">
+    
+    EXAMPLE> (run) ; run all tests
+    BAR-TEST.
+    FOO-TEST..
+    Success: 2 tests, 3 checks.
+    
+    EXAMPLE> (foo-test) ; tests are just functions
+    FOO-TEST..
+    Success: 1 test, 2 checks.
+    
+    EXAMPLE> (run '(bar-test)) ; run particular tests
+    BAR-TEST.
+    Success: 1 test, 1 check.
+    
+    EXAMPLE> (setf *tests* '(foo-test)) ; set the default tests to run
+    (FOO-TEST)
+    EXAMPLE> (run)
+    FOO-TEST..
+    Success: 1 test, 2 checks.
+
+## Overview
+
+1am is tiny (~60 lines of code), has no dependencies, and will be
+stable for the foreseeable future. It was originally written for large
+multi-threaded test suites where the complexity of popular testing
+frameworks caused problems. The philosophy of 1am is that simple is
+better. 1am is especially tailored for fixing bugs that are difficult
+to reproduce.
+
+* A test failure always results in a breakpoint with backtrace
+  (otherwise a rare opportunity may be missed).
+
+* The order of tests is shuffled on each run so that bugs concealed by
+  unintentional dependencies between tests may be exposed.
+
+* If a test run fails to complete then subsequent runs will use the
+  same random state that led to the failure, until a successful run.
+
+* Checks may occur inside threads.
+
+* 1am's negligible overhead is unlikely to bury timing bugs and race
+  conditions in the code being tested.
+
+* Tests are runnable as ordinary functions of the same name.
+
+* Tests are compiled up front (some testing frameworks, such as 5am,
+  defer compilation by default).
+
+* The generated code is small; compiling tests is ~8x faster than 5am
+  configured with `:compile-at :definition-time`. Compiling large-ish
+  tests with 5am can cause the default heap size to be exceeded on
+  some platforms (32-bit SBCL, Allegro Express).
+
+* Type inferencing (if present) works inside the `is` macro.
+
+* Since 1am is small and stable, copying it into the project being
+  tested is a viable option (be sure to use a different package name).
+
+## Workflow
+
+Instead of a hierarchy of test suites, 1am has a flat list of tests in
+`*tests*`. When a test is compiled, it is added to `*tests*`. A
+typical workflow might be:
+
+1. Run all tests to verify they pass.
+
+2. Set `*tests*` to `nil`, then compile specific tests that are
+   relevant to some feature, perhaps writing new tests in the process.
+   Now `*tests*` contains only those tests.
+
+3. When changes for that feature are complete, recompile all tests to
+   confirm those changes, which will add them back to `*tests*`.
+
+4. Run all tests again to check that nothing is messed up.
+
+## API
+
+* [special variable] **`*tests*`** -- A list of tests; the default
+  argument to `run`.
+
+* [macro] **`test`** `name` *`&body`* `body` -- Define a test function
+  and add it to `*tests*`.
+
+* [macro] **`is`** `form` -- Assert that `form` evaluates to non-nil.
+
+* [macro] **`signals`** `condition` *`&body`* `body` -- Assert that
+  `body` signals a condition of type `condition`.
+
+* [function] **`run`** *`&optional`* `tests` -- Run each test in the
+  sequence `tests`. Default is `*tests*`.
